7aabe0c70ca128f0ce729e377977b0c1b3d7f6f0
lets-code-daily <kunal.soulrocker@gmail.com>
2024-06-17 07:15:30 +0530

    project structure

--- template.py
+++ template.py
@@ -2,27 +2,30 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name='Data-Science-Project'
+project_name='Text-Summarizer'
 
 list_of_files = [
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init.py",
-    f"src/{project_name}/components/data_ingestion.py",
-    f"src/{project_name}/components/data_transformation.py",
-    f"src/{project_name}/components/model_trainer.py",
-    f"src/{project_name}/components/model_monitoring.py",
-    f"src/{project_name}/piplines/__init__.py",
-    f"src/{project_name}/piplines/training_pipeline.py",
-    f"src/{project_name}/piplines/prediction_pipeline.py",
-    f"src/{project_name}/exception.py",
-    f"src/{project_name}/logger.py",
-    f"src/{project_name}/utils.py",
+    f"src/{project_name}/conponents/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/logging/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
     "app.py",
+    "main.py",
     "Dockerfile",
     "requirements.txt",
-    "setup.py"
+    "setup.py",
+    "research/trials.ipynb"
 ]
 
 for filepath in list_of_files:
